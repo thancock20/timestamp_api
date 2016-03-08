c0e1743be5e6060b805ce5ffcb14b7cb36fb247b
Todd Hancock <thancock20@hotmail.com>
2016-03-07 19:25:43 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var moment = require('moment');
+moment().utc();
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+router.get('/:date', function(req, res) {
+  var timestamp = moment.unix(req.params.date);
+  timestamp = timestamp.isValid() ? timestamp : moment(new Date(req.params.date));
+  if (timestamp.isValid()) {
+    res.json({
+      unix: moment(timestamp).format('X'),
+      natural: moment(timestamp).format('MMMM D, YYYY')
+    })
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    })
+  }
+});
+
+app.use('/', router);
+
+app.listen(port);
